bf1c0a6f2b6fb3e40b0083fd0bb6abda1bc76f1d
Philippe Faist <phfaist@gmail.com>
2022-11-30 12:33:57 +0100

    fixes to $<preset> directives in yaml

--- llm_citations/__init__.py
+++ llm_citations/__init__.py
@@ -3,14 +3,14 @@ llm_default_import_config = {
     'llm': {
         'features': [
             {
-                '$preset': 'defaults',
+                '$defaults': True
             },
             {
                 'name': 'llm_citations.feature_cite.FeatureCiteAuto',
                 'config': {
                     'sources': [
                         {
-                            '$preset': 'defaults',
+                            '$defaults': True,
                         },
                     ],
                 },
