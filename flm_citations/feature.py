557aa73eab8473dc4a33a578d8088011ed9a3898
Philippe Faist <phfaist@gmail.com>
2023-04-15 18:02:33 +0200

    use original citeproc-py repo, hopefully they will merge my pr soon...

--- flm_citations/feature.py
+++ flm_citations/feature.py
@@ -328,8 +328,9 @@ def _generate_citation_flm_from_citeprocjsond(
         )
 
     with warnings.catch_warnings():
-        warnings.simplefilter('ignore', citeproc.source.MissingArgumentWarning)
-        warnings.simplefilter('ignore', citeproc.source.UnsupportedArgumentWarning)
+        # warnings.simplefilter('ignore', citeproc.source.MissingArgumentWarning)
+        # warnings.simplefilter('ignore', citeproc.source.UnsupportedArgumentWarning)
+        warnings.simplefilter('ignore', UserWarning) # until citeproc-py merges my PR
 
         citekey = citeprocjsond['id']
 
